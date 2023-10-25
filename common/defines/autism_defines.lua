d9ba6ec923e89a95ec0a58d18be2d1e8ecc0ef1f
Mumiago <adam.gatia@gmail.com>
2023-10-25 16:15:22 +0200

    2

--- common/defines/autism_defines.lua
+++ common/defines/autism_defines.lua
@@ -2,7 +2,7 @@ NDefines.NGame.LAG_DAYS_FOR_LOWER_SPEED = 250
 NDefines.NGame.LAG_DAYS_FOR_PAUSE = 200
 NDefines.NGame.COMBAT_LOG_MAX_MONTHS = 36
 NDefines.NCountry.EVENT_PROCESS_OFFSET = 40 -- Performance enhancer. --TW/WTT
-NDefines.NGame.GAME_SPEED_SECONDS = { 600.0, 0.27 , 0.23, 0.17, 0.09 }
+NDefines.NGame.GAME_SPEED_SECONDS = { 600.0, 0.29 , 0.26, 0.19, 0.11 }
 ---Diplomacy
 NDefines.NDiplomacy.PEACE_SCORE_PER_PASS = 2.0
 NDefines.NDiplomacy.DIPLOMACY_HOURS_BETWEEN_REQUESTS = 12
