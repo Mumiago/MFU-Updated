0fd44a89f79c8e20bd086e8fbca36cb45744a56b
Mumiago <adam.gatia@gmail.com>
2024-02-15 12:03:05 +0100

    .

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -2375,9 +2375,9 @@ NCharacter = {
 	COUNTRY_LEADER_BASE_EXPIRE_YEAR_LENGTH = 5, -- When creating a dynamic country leader if an expire date is not set it will have 5 years as a base expiration date
 	COUNTRY_LEADER_BASE_RANDOM_MAX_YEAR_LENGTH = 15, -- Max random value added to COUNTRY_LEADER_BASE_EXPIRE_YEAR_LENGTH
 
-	SPECIALIST_ADVISOR_MIN_RANK = 3,
-	EXPERT_ADVISOR_MIN_RANK = 5,
-	GENIUS_ADVISOR_MIN_RANK = 7,
+	SPECIALIST_ADVISOR_MIN_RANK = 2,
+	EXPERT_ADVISOR_MIN_RANK = 4,
+	GENIUS_ADVISOR_MIN_RANK = 6,
 },
 NAITheatre = {
 	AI_THEATRE_GENERATION_HOME_THEATRE_DEPTH_RESTRICTION = 2,			-- The home theatre is generated based off a initial depth restriction
