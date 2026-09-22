f1c92a7bc5b63ac5216f9a74d91ad4e37f9d3ea3
autumnequinox <142721663+autumnequinox78644@users.noreply.github.com>
2026-09-22 13:34:54 -0700

    pushing cause rulo asked
    -some xp focuses were changed on germany
    - germany got some better artillery buffs(not tank cannons)
    - ideological loyalty buffed since new spirits

--- common/defines/00_defines.lua
+++ common/defines/00_defines.lua
@@ -867,7 +867,7 @@ NTechnology = {
 	BASE_RESEARCH_POINTS_SAVED = 25.0,		-- Base amount of research points a country can save per slot.
 	BASE_YEAR_AHEAD_PENALTY_FACTOR = 4,		-- 3 Base year ahead penalty
 	BASE_TECH_COST = 78,					-- base cost for a tech. multiplied with tech cost and ahead of time penalties
-	MAX_TECH_SHARING_BONUS = 1, 			-- Max technology sharing bonus that can be applied instantly
+	MAX_TECH_SHARING_BONUS = 0.5, 			-- Max technology sharing bonus that can be applied instantly
 	LICENSE_PRODUCTION_TECH_BONUS = 0.10,	-- License production tech bonus
  
 	DEFAULT_XP_RESEARCH_COST = 100,			-- default xp cost of a research to speed up the process
